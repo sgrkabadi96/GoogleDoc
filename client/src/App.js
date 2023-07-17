5ec8286c40d7525620a94b149be6aa02e3422c58
Sagar <sgrkabadi96@gmail.com>
2023-07-17 23:32:19 +0530

    first

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { v4 as uuidV4 } from 'uuid';
+import TextEditor from './TextEditor';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Navigate to={`/documents/${uuidV4()}`} />} />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </Router> 
+  );
+}
+
+export default App;
